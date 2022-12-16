6bcb9714e2f66facd9ffa4af258ca21e83c64ac1
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2022-12-16 22:34:25 +0600

    Add files via upload

---
+++ BashScripting/first_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "I will be a linux expert"
+
+#echo $BASH
+
+name="Chayan"
+
+echo "Hello ${name},plear enter your age "
+
+read age
+
+echo "my age is ${age}"
+
+echo "Friend :Hello"
+
+sleep 2
+
+echo "Me: Hi What's up $1 "
+
+sleep 2
+
+echo "Thanks for messageing me"
+
---
+++ BashScripting/if_statement.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ "$1" = "chayan" ]
+then
+ echo "Wellcome $1"
+else
+ echo "Thanks"
+fi
